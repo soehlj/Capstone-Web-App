1e846f14efecd702d703713c9ad38265bf51524b
soehlj <jdsoehl@yahoo.com>
2025-11-20 16:15:16 -0800

    fixed about path

--- app.py
+++ app.py
@@ -5,11 +5,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Create the Flask application instance
 app = Flask(__name__)
+Scss(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+#Row of data
+#class Maindata(db.Model):
+    #Finish Late
+    
 # Define a route (homepage)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 # Run the app
 if __name__ == "__main__":
